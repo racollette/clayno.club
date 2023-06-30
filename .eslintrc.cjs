ea46aa7ac84a60be39dbd498e9686368efffb52f
racollette <racollette@gmail.com>
2023-06-30 00:33:23 -0400

    eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,8 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off"
   },
 };
 
