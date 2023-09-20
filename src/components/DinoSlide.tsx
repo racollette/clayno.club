d3159403df832b368c9c8604f857055a925b95fb
racollette <racollette@gmail.com>
2023-09-19 20:29:04 -0400

    add PFP toggle

--- src/components/DinoSlide.tsx
+++ src/components/DinoSlide.tsx
@@ -2,7 +2,8 @@ import { useUser } from "~/hooks/useUser";
 import { api } from "~/utils/api";
 import Image from "next/image";
 import { HiArrowCircleUp } from "react-icons/hi";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import ToggleSwitch from "./ToggleSwitch";
 
 type DinoSlideProps = {
   handlePlace: (imageURL: string, motion: string, mint: string) => void;
@@ -19,6 +20,7 @@ export default function DinoSlide({
   handleDragStart,
 }: DinoSlideProps) {
   const { user, session, isLoading } = useUser();
+  const [showPFP, setShowPFP] = useState(false);
 
   const wallets = user?.wallets.map((wallet: any) => wallet.address) ?? [];
 
@@ -26,11 +28,23 @@ export default function DinoSlide({
     wallets: wallets,
   });
 
+  const togglePFP = (newToggleState: boolean) => {
+    setShowPFP(newToggleState);
+  };
+
   return (
     <div className="fixed bottom-0 left-0 h-48 w-full overflow-y-scroll border-t border-stone-400 bg-stone-600">
       <div className="flex p-4">
         {user && session ? (
           <div>
+            <div>
+              <ToggleSwitch
+                className="justify-self-start"
+                toggleState={showPFP}
+                label={"PFP Mode"}
+                onToggle={togglePFP}
+              />
+            </div>
             <div className="flex flex-row flex-wrap gap-2">
               {holders?.map((holder) => (
                 <Fragment key={holder.owner}>
@@ -40,7 +54,9 @@ export default function DinoSlide({
                       className="relative flex h-36 w-36 cursor-grab justify-center overflow-clip rounded-md"
                     >
                       <Image
-                        src={`https://prod-image-cdn.tensor.trade/images/slug=claynosaurz/400x400/freeze=false/${dino.gif}`}
+                        src={`https://prod-image-cdn.tensor.trade/images/slug=claynosaurz/400x400/freeze=false/${
+                          showPFP ? dino.pfp : dino.gif
+                        }`}
                         alt=""
                         fill
                         quality={75}
@@ -56,8 +72,8 @@ export default function DinoSlide({
                       <div
                         onClick={() =>
                           handlePlace(
-                            dino.gif,
-                            dino.attributes?.motion || "",
+                            showPFP ? dino.pfp : dino.gif,
+                            showPFP ? "PFP" : dino.attributes?.motion || "None",
                             dino.mint
                           )
                         }
