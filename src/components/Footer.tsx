5730c507f7aaf4cb1adf31a13b1eb04d023f075c
racollette <racollette@gmail.com>
2023-09-30 00:25:49 -0400

    center text vertically

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center bg-black pb-6 text-center text-white md:text-lg">
-      <span className="flex flex-row gap-2">
-        <span className="self-center text-sm">Made with</span>
+      <div className="flex items-center justify-center gap-2">
+        <span className="text-sm">Made with</span>
         <Image src="/icons/heart.svg" alt="Love" width={16} height={16} />
-        <span className="self-center text-sm">by</span>
+        <span className="text-sm">by</span>
         <Link
           href={`https://twitter.com/AlphaDecay235`}
-          className="self-center text-sky-500"
+          className="text-sm text-sky-500"
           target="_blank"
         >
-          <span className="self-center text-sm">@AlphaDecay235</span>
+          <span className="text-sm">@AlphaDecay235</span>
         </Link>
-      </span>
+      </div>
     </footer>
   );
 }
