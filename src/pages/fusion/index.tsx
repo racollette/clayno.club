1adc5bbc34e5bb08a0832c057937e44f71b845c6
racollette <racollette@gmail.com>
2023-10-15 11:19:39 -0400

    mobile styling

--- src/pages/fusion/index.tsx
+++ src/pages/fusion/index.tsx
@@ -366,7 +366,7 @@ export default function FusionPage() {
               : `w-full lg:w-1/2 xl:w-1/3 2xl:w-2/5`
           } select-none flex-col items-start justify-center gap-y-4`}
         >
-          <div className="flex flex-row flex-wrap justify-start gap-4 rounded-lg bg-neutral-800 px-4 py-2">
+          <div className="flex flex-row flex-wrap justify-start gap-2 rounded-lg bg-neutral-800 px-4 py-2 md:gap-4">
             <TooltipProvider>
               <CollageModal
                 title="My Collages"
