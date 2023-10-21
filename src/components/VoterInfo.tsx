b7a5ee43168a3674e0a666242dae03f687c7dd73
racollette <racollette@gmail.com>
2023-10-21 16:22:50 -0400

    add session check to voter widget

--- src/components/VoterInfo.tsx
+++ src/components/VoterInfo.tsx
@@ -1,15 +1,16 @@
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useUser } from "~/hooks/useUser";
 
 export default function VoterInfo() {
   const { user, voterInfo, voterInfoLoading } = useUser();
+  const { status } = useSession();
+
+  console.log(status);
 
   return (
     <>
-      {!voterInfoLoading &&
-      voterInfo &&
-      user &&
-      voterInfo?.votesAvailable >= 0 ? (
+      {status === "authenticated" && !voterInfoLoading && voterInfo && user ? (
         <div className="flex items-center justify-center rounded-lg bg-fuchsia-700 px-3 py-2">
           <span className="text-xs font-bold">
             {voterInfo.votesAvailable} Votes
