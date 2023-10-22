6f9e1fc4fa60f19e8615459669ebfb784e4f4dd2
racollette <racollette@gmail.com>
2023-10-22 17:09:50 -0400

    remove sessionType

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,18 +1,12 @@
 import type { Session } from "next-auth/core/types";
 
 export const getSessionDetails = (session: Session | null) => {
-  const sessionType = session?.user?.image?.includes("discordapp")
-    ? "discord"
-    : session?.user?.image?.includes("twimg")
-    ? "twitter"
-    : "wallet";
-
   const id =
-    sessionType === "twitter"
+    session?.user.type === "twitter"
       ? session?.user.profile.data.username
-      : sessionType === "discord"
+      : session?.user.type === "discord"
       ? session?.user.profile.username
       : session?.user.name;
 
-  return { sessionType, id };
+  return { id };
 };
