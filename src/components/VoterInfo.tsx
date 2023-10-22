7a47ec171e70f4c450e4e77641423eb883274612
racollette <racollette@gmail.com>
2023-10-22 17:23:58 -0400

    switch voterinfo to happy rex icon

--- src/components/VoterInfo.tsx
+++ src/components/VoterInfo.tsx
@@ -1,5 +1,6 @@
 import { useSession } from "next-auth/react";
 import { useUser } from "~/hooks/useUser";
+import Image from "next/image";
 
 export default function VoterInfo() {
   const { user, voterInfo, voterInfoLoading } = useUser();
@@ -8,10 +9,15 @@ export default function VoterInfo() {
   return (
     <>
       {status === "authenticated" && !voterInfoLoading && voterInfo && user ? (
-        <div className="flex items-center justify-center rounded-lg bg-fuchsia-700 px-3 py-2">
-          <span className="text-xs font-bold">
-            {voterInfo.votesAvailable} Votes
-          </span>
+        <div className="flex flex-row items-center justify-center gap-1 rounded-lg bg-fuchsia-700 px-4 py-2">
+          <span className="text-sm font-bold">{voterInfo.votesAvailable}</span>
+          <Image
+            src={"/images/rex_happy.png"}
+            alt="RAWR"
+            width={20}
+            height={20}
+            className="scale-x-[-1] transform"
+          />
         </div>
       ) : null}
     </>
