011afc04ce4f68ef62e816f3f964912e7e2a2c40
racollette <racollette@gmail.com>
2023-10-24 00:01:37 -0400

    add keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,10 +66,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "homepage-dino-pattern": {
+          "0%": { transform: "translateY(0px)" },
+          "100%": { transform: "translateY(1800px)" },
+        },
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "homepage-dino-pattern": "homepage-dino-pattern 60s linear infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
       },
     },
   },
