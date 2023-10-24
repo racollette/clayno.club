ae4eeeb5e64d227041d54df3badc6f70e75adc2e
racollette <racollette@gmail.com>
2023-10-24 00:03:22 -0400

    landing page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,47 +20,50 @@ export default function Home() {
             className="h-[5000px] w-[5000px] animate-homepage-dino-pattern bg-repeat"
           ></div>
         </div>
-        <div className="z-10 flex w-full flex-col items-center justify-around gap-10 md:flex-row">
+        <div className="z-10 my-16 flex w-full flex-col items-center justify-around gap-16 md:flex-row md:gap-10">
           <Link
-            className="flex aspect-square w-1/3 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center text-white  shadow-2xl shadow-neutral-900 hover:animate-wiggle md:w-1/5"
+            className="flex aspect-square w-3/4 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center text-white  shadow-2xl shadow-neutral-900 hover:animate-wiggle md:w-1/5 lg:w-1/4"
             style={{
-              background: `url('/gifs/guesty_herd.mp4')`,
-              // backgroundSize: `100%`,
+              background: `url('/images/trailer_herd.jpeg')`,
+              backgroundSize: `100%`,
             }}
             href={`/herds`}
           >
-            <h1 className="rounded-lg  bg-neutral-900/70 p-2 text-3xl font-black">
+            <h1 className="rounded-lg  bg-neutral-900/70 p-2 text-2xl font-black lg:text-3xl">
               Herds
             </h1>
-            <h2 className="rounded-lg bg-neutral-900/70 p-2 text-sm text-white">
-              Collectors Gallery
+            <h2 className="lg:text-md rounded-lg bg-neutral-900/70 p-2 text-sm font-semibold text-white">
+              Collectors gallery
             </h2>
           </Link>
           <Link
-            className="flex aspect-square w-1/3 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 p-8 text-center text-white shadow-2xl shadow-neutral-900  hover:animate-wiggle md:w-1/5"
-            style={{ background: `url('/images/tribes_banner.png')` }}
+            className="flex aspect-square w-3/4 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center text-white shadow-2xl  shadow-neutral-900 hover:animate-wiggle md:w-1/4"
+            style={{
+              background: `url('/images/SMALLER_FJORD_CLAYNOS.jpg')`,
+              backgroundSize: `100%`,
+            }}
             href={`/tribes`}
           >
-            <h1 className="rounded-lg  bg-neutral-900/70 p-2 text-3xl font-black">
+            <h1 className="rounded-lg  bg-neutral-900/70 p-2 text-2xl font-black lg:text-3xl">
               Tribes
             </h1>
-            <h2 className="rounded-lg bg-neutral-900/70 p-2 text-sm text-white">
-              Sub-Communities within the world of Claynotopia!
+            <h2 className="lg:text-md rounded-lg bg-neutral-900/70 p-2 text-sm font-semibold text-white">
+              Claynotopia's sub-communities!
             </h2>
           </Link>
           <Link
-            className="flex aspect-square w-1/3 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center  text-white shadow-2xl shadow-neutral-900 hover:animate-wiggle md:w-1/5"
+            className="flex aspect-square w-3/4 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center  text-white shadow-2xl shadow-neutral-900 hover:animate-wiggle md:w-1/5 lg:w-1/4"
             style={{
               background: `url('/images/sticker_collage.png')`,
               backgroundSize: `100%`,
             }}
             href={`/fusion`}
           >
-            <h1 className="rounded-lg  bg-neutral-900/70 p-2 text-3xl font-black">
+            <h1 className="rounded-lg  bg-neutral-900/70 p-2 text-2xl font-black lg:text-3xl">
               Fusion
             </h1>
-            <h2 className="rounded-lg bg-neutral-900/70 p-2 text-sm text-white">
-              A dino NFT video collage generation tool
+            <h2 className="lg:text-md rounded-lg bg-neutral-900/70 p-2 text-sm font-semibold text-white">
+              An NFT video collage creation tool
             </h2>
           </Link>
         </div>
