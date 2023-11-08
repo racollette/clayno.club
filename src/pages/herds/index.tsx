ea41d98e811a077977b42786e81cec011e6c0e9b
racollette <racollette@gmail.com>
2023-11-07 22:30:24 -0500

    import as type

--- src/pages/herds/index.tsx
+++ src/pages/herds/index.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useTimeSinceLastUpdate } from "~/hooks/useUpdated";
 import { useRouter, useSearchParams } from "next/navigation";
-import {
+import type {
   Herd as HerdType,
   Attributes,
   Dino,
