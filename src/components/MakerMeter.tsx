90353eaba55f77903e1e44194da44871b80cca0a
racollette <racollette@gmail.com>
2023-11-10 20:17:29 -0500

    maker charges meter

---
+++ src/components/MakerMeter.tsx
@@ -0,0 +1,178 @@
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { ORIGINAL_CLAYMAKER_SUPPLY } from "~/utils/constants";
+import { api } from "~/utils/api";
+
+export const MakerMeter = () => {
+  const clayBarRef = useRef<HTMLDivElement>(null);
+  const [clayBarWidth, setClayBarWidth] = useState(0);
+
+  const {
+    data: claymakerCounts,
+    // isLoading,
+    // refetch,
+  } = api.stats.getMakerChargesSnapshot.useQuery();
+
+  const updateClayBarWidth = () => {
+    if (clayBarRef.current) {
+      setClayBarWidth(clayBarRef.current.offsetWidth);
+    }
+  };
+
+  useEffect(() => {
+    // Initial update
+    updateClayBarWidth();
+
+    // Listen for the window resize event
+    window.addEventListener("resize", updateClayBarWidth);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", updateClayBarWidth);
+    };
+  }, []);
+
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <h1 className="p-4 text-center font-claynoShadow text-3xl md:text-5xl">
+        <span className="text-blue-400">Ma</span>
+        <span className="mr-2 text-yellow-300">ker</span>
+        <span className="ml-2 text-green-400">Ch</span>
+        <span className=" text-purple-500">ar</span>
+        <span className="text-pink-500">ges</span>
+      </h1>
+      {claymakerCounts && (
+        <>
+          <div className="container flex flex-col gap-2 font-clayno">
+            <div className="flex flex-row gap-2">
+              <Image
+                src="/images/first_claymaker.gif"
+                width={40}
+                height={40}
+                alt="First Edition Claymaker"
+                className="rounded-full"
+              />
+              <div
+                className="relative flex h-10 w-full items-center overflow-clip rounded-3xl border-2 border-red-500 bg-red-500/25"
+                ref={clayBarRef}
+              >
+                <div
+                  className="h-full min-w-[50px] rounded-r-3xl bg-red-500"
+                  style={{
+                    width: `${Math.floor(
+                      ((ORIGINAL_CLAYMAKER_SUPPLY.first -
+                        claymakerCounts.first) /
+                        ORIGINAL_CLAYMAKER_SUPPLY.first) *
+                        clayBarWidth
+                    )}px`,
+                  }}
+                ></div>
+                <p className="absolute left-4 text-lg font-extrabold">
+                  {ORIGINAL_CLAYMAKER_SUPPLY.first - claymakerCounts.first}
+                </p>
+                <p className="absolute left-1/2  text-lg font-extrabold">
+                  {(
+                    ((ORIGINAL_CLAYMAKER_SUPPLY.first - claymakerCounts.first) /
+                      ORIGINAL_CLAYMAKER_SUPPLY.first) *
+                    100
+                  ).toFixed(2)}
+                  %
+                </p>
+
+                <p className="absolute right-4 text-lg font-extrabold">
+                  {claymakerCounts.first}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="container flex flex-col gap-2 font-clayno">
+            <div className="flex flex-row gap-2">
+              <Image
+                src="/images/deluxe_claymaker.gif"
+                width={40}
+                height={40}
+                alt="Deluxe Edition Claymaker"
+                className="rounded-full"
+              />
+              <div
+                className="items-centeroverflow-clip relative flex h-10 w-full items-center overflow-clip rounded-3xl border-2 border-red-500 bg-red-500/25"
+                ref={clayBarRef}
+              >
+                <div
+                  className="h-full min-w-[50px] rounded-r-3xl bg-red-500"
+                  style={{
+                    width: `${Math.floor(
+                      ((ORIGINAL_CLAYMAKER_SUPPLY.deluxe -
+                        claymakerCounts.deluxe) /
+                        ORIGINAL_CLAYMAKER_SUPPLY.deluxe) *
+                        clayBarWidth
+                    )}px`,
+                  }}
+                ></div>
+                <p className="absolute left-4 text-lg font-extrabold">
+                  {ORIGINAL_CLAYMAKER_SUPPLY.deluxe - claymakerCounts.deluxe}
+                </p>
+                <p className="absolute left-1/2  text-lg font-extrabold">
+                  {(
+                    ((ORIGINAL_CLAYMAKER_SUPPLY.deluxe -
+                      claymakerCounts.deluxe) /
+                      ORIGINAL_CLAYMAKER_SUPPLY.deluxe) *
+                    100
+                  ).toFixed(2)}
+                  %
+                </p>
+
+                <p className="absolute right-4 text-lg font-extrabold">
+                  {claymakerCounts.deluxe}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="container flex flex-col gap-2 font-clayno">
+            <div className="flex flex-row gap-2">
+              <Image
+                src="/images/limited_claymaker.gif"
+                width={40}
+                height={40}
+                alt="Limited Edition Claymaker"
+                className="rounded-full"
+              />
+              <div
+                className="items-centeroverflow-clip relative flex h-10 w-full items-center overflow-clip rounded-3xl border-2 border-red-500 bg-red-500/25"
+                ref={clayBarRef}
+              >
+                <div
+                  className="h-full min-w-[50px] rounded-r-3xl bg-red-500"
+                  style={{
+                    width: `${Math.floor(
+                      ((ORIGINAL_CLAYMAKER_SUPPLY.limited -
+                        claymakerCounts.limited) /
+                        ORIGINAL_CLAYMAKER_SUPPLY.limited) *
+                        clayBarWidth
+                    )}px`,
+                  }}
+                ></div>
+                <p className="absolute left-4 text-lg font-extrabold">
+                  {ORIGINAL_CLAYMAKER_SUPPLY.limited - claymakerCounts.limited}
+                </p>
+                <p className="absolute left-1/2  text-lg font-extrabold">
+                  {(
+                    ((ORIGINAL_CLAYMAKER_SUPPLY.limited -
+                      claymakerCounts.limited) /
+                      ORIGINAL_CLAYMAKER_SUPPLY.limited) *
+                    100
+                  ).toFixed(2)}
+                  %
+                </p>
+
+                <p className="absolute right-4 text-lg font-extrabold">
+                  {claymakerCounts.limited}
+                </p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
