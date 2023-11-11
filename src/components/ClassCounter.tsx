6c299e0b85b5b80fed594f6039b47f19425cd305
racollette <racollette@gmail.com>
2023-11-11 11:15:23 -0500

    class counter

--- src/components/ClassCounter.tsx
+++ src/components/ClassCounter.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { api } from "~/utils/api";
 import { useWidth } from "~/hooks/useWidth";
+import { CLAY_CLASS_RESOURCES } from "~/utils/constants";
 
 type ClassItem = {
   Stego: number;
@@ -28,7 +29,9 @@ function getBgColor(species: string) {
 }
 
 function calculateWidth(elementWidth: number, count: number, total: number) {
-  return `${(count / total) * elementWidth * 0.925}px`;
+  const containerOffset =
+    elementWidth > 768 ? elementWidth * 0.95 : elementWidth;
+  return `${(count / total) * containerOffset}px`;
 }
 
 export const ClassCounter = () => {
@@ -48,15 +51,17 @@ export const ClassCounter = () => {
     setTopClass(largestTotal);
   }, [data]);
 
+  const [isWiggling, setIsWiggling] = useState(null);
+
   return (
     <div className="flex w-full flex-col gap-2">
-      <h1 className="p-4 text-center font-claynoShadow text-3xl md:text-5xl">
+      <h1 className="p-4 text-center font-clayno text-3xl hover:animate-wiggle md:text-5xl">
         <span className="text-green-500">Cl</span>
         <span className="text-blue-400">as</span>
         <span className="text-red-400">ses</span>
       </h1>
       <div
-        className="container flex w-full flex-col items-center gap-8"
+        className="flex w-full flex-col items-center gap-8 md:container"
         ref={containerWidth}
       >
         {data &&
@@ -72,7 +77,7 @@ export const ClassCounter = () => {
               return 0;
             })
             .map((classItem: { [key: string]: ClassItem }) => {
-              const classType = Object.keys(classItem)[0];
+              const classType = Object.keys(classItem)[0] ?? "warrior";
               const speciesList = Object.values(classItem)[0];
               const total = speciesList?.Total as number;
 
@@ -105,40 +110,88 @@ export const ClassCounter = () => {
                           return (
                             <div
                               key={species[0]}
-                              className={`flex min-w-fit flex-row items-center justify-center gap-1 px-4 font-clayno  ${getBgColor(
+                              className={`flex min-w-fit cursor-pointer flex-row items-center justify-center px-2 font-clayno hover:animate-pulse md:gap-1 md:px-4  ${getBgColor(
                                 species[0]
                               )}`}
                               style={{
                                 width: `${
                                   speciesList
                                     ? (species[1] / total) * 100
-                                    : `33.333`
+                                    : `33.33333`
                                 }%`,
                               }}
                             >
-                              <p>{species[1]}</p>
-                              <Image
-                                src={`/icons/${species[0].toLowerCase()}_colored.svg`}
-                                width={50}
-                                height={50}
-                                alt={`${species[0]} Icon`}
-                              />
+                              <p className="md:text-md text-sm">{species[1]}</p>
+                              <div
+                                className={`relative h-[35px] w-[35px] hover:animate-wiggle md:h-[50px] md:w-[50px] `}
+                              >
+                                <Image
+                                  src={`/icons/${species[0].toLowerCase()}_colored.svg`}
+                                  fill
+                                  alt={`${species[0]} Icon`}
+                                />
+                              </div>
                               <p className="hidden md:block">{species[0]}</p>
                             </div>
                           );
                         })}
                     </div>
                   </div>
-                  <div className="flex h-[50px] flex-row items-center gap-2 bg-gray-300">
-                    <p className="whitespace-nowrap px-4 text-right font-clayno text-black">
-                      {total} {classType}s
+                  <div className="relative flex h-[35px] flex-row items-center gap-2 rounded-r-full bg-gray-300 md:h-[50px]">
+                    <p className="flex flex-row flex-nowrap gap-2 whitespace-nowrap px-4 text-right font-clayno text-black">
+                      <span>{total}</span>
+                      <span className="hidden md:block">{classType}s</span>
                     </p>
-                    <Image
-                      src={`/images/${classType?.toLowerCase()}.png`}
-                      width={75}
-                      height={75}
-                      alt={`Class ${classType} Placeholder`}
-                    />
+                    <div className="relative h-[55px] w-[55px] md:h-[75px] md:w-[75px]">
+                      <Image
+                        src={`/images/${classType?.toLowerCase()}.png`}
+                        fill
+                        alt={`Class ${classType} Placeholder`}
+                      />
+                      <div className="relative flex cursor-pointer items-center justify-center opacity-0 hover:opacity-100">
+                        <div className="relative h-[55px] w-[55px] animate-spin-slow md:h-[75px] md:w-[75px]">
+                          <div className="absolute -top-8 left-3 h-[30px] w-[30px] md:-top-14 md:left-5 md:h-[50px] md:w-[50px]">
+                            <Image
+                              src={`/images/${
+                                CLAY_CLASS_RESOURCES[
+                                  classType?.toLowerCase()
+                                ]?.[0]
+                              }.png`}
+                              alt={`Clay`}
+                              fill
+                              className="rounded-full"
+                            />
+                          </div>
+                          <div className="absolute right-14 top-3 h-[30px] w-[30px] md:left-20 md:top-3 md:h-[50px] md:w-[50px]">
+                            <Image
+                              src={`/images/${
+                                CLAY_CLASS_RESOURCES[
+                                  classType?.toLowerCase()
+                                ]?.[1]
+                              }.png`}
+                              alt={`Clay`}
+                              fill
+                              className="rounded-full"
+                            />
+                          </div>
+                          <div className="absolute left-3 top-14 h-[30px] w-[30px] md:left-5 md:top-20 md:h-[50px] md:w-[50px]">
+                            <Image
+                              src={`/images/${
+                                CLAY_CLASS_RESOURCES[
+                                  classType?.toLowerCase()
+                                ]?.[2]
+                              }.png`}
+                              alt={`Clay`}
+                              fill
+                              className="rounded-full"
+                            />
+                          </div>
+                        </div>
+                        <h1 className="absolute right-0 top-4 animate-none rounded-lg bg-black p-1 font-clayno text-xs md:hidden">
+                          {classType}
+                        </h1>
+                      </div>
+                    </div>
                   </div>
                 </div>
               );
