724618065fd56a2a1641278e528b482ea2b8f9c1
racollette <racollette@gmail.com>
2023-11-21 18:42:06 -0500

    holder table query

--- src/server/api/routers/stats.ts
+++ src/server/api/routers/stats.ts
@@ -29,4 +29,65 @@ export const statsRouter = createTRPCRouter({
       },
     });
   }),
+
+  getDinoHolders: publicProcedure.query(async ({ ctx }) => {
+    const ogHolders = await ctx.prisma.dino.groupBy({
+      by: ["holderOwner"],
+      _count: {
+        mint: true,
+      },
+      where: {
+        holderOwner: { not: null },
+        NOT: {
+          attributes: {
+            species: { in: ["Spino", "Para"] },
+          },
+        },
+      },
+      orderBy: {
+        _count: {
+          mint: "desc",
+        },
+      },
+    });
+
+    const sagaHolders = await ctx.prisma.dino.groupBy({
+      by: ["holderOwner"],
+      _count: {
+        mint: true,
+      },
+      where: {
+        holderOwner: { not: null },
+        attributes: {
+          species: { in: ["Spino", "Para"] },
+        },
+      },
+      orderBy: {
+        _count: {
+          mint: "desc",
+        },
+      },
+    });
+
+    const combinedHolders: {
+      holderOwner: string | null;
+      ogCount: number;
+      sagaCount: number;
+    }[] = [];
+
+    // Combine data based on holderOwner (address)
+    ogHolders.forEach((ogHolder) => {
+      const matchingSagaHolder = sagaHolders.find(
+        (sagaHolder) => sagaHolder.holderOwner === ogHolder.holderOwner
+      );
+
+      combinedHolders.push({
+        holderOwner: ogHolder.holderOwner,
+        ogCount: ogHolder._count.mint,
+        sagaCount: matchingSagaHolder ? matchingSagaHolder._count.mint : 0,
+      });
+    });
+
+    return combinedHolders.sort((a, b) => b.ogCount - a.ogCount);
+  }),
 });
