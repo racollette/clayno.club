d8017ff307d499d91df2dd762a65fd0c790f8689
racollette <racollette@gmail.com>
2023-11-22 22:44:35 -0500

    convert stats page to tabs

--- src/pages/stats/index.tsx
+++ src/pages/stats/index.tsx
@@ -3,6 +3,13 @@ import Head from "next/head";
 import { MoldedMeter } from "~/components/MoldedMeter";
 import { ClassCounter } from "~/components/ClassCounter";
 import { MakerMeter } from "~/components/MakerMeter";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "~/@/components/ui/tabs";
+import Holders from "./holders";
 
 export default function StatsPage() {
   return (
@@ -11,10 +18,34 @@ export default function StatsPage() {
         <title>DinoHerd | Stats</title>
       </Head>
       <Layout>
-        <section className="flex w-full flex-col items-center justify-center gap-12 overflow-hidden py-4 md:items-start md:px-4 md:py-8">
-          <ClassCounter />
-          <MoldedMeter />
-          <MakerMeter />
+        <section className="flex w-full flex-col items-center overflow-hidden py-4 md:px-4 md:py-8">
+          <Tabs
+            defaultValue="resources"
+            className="flex w-full flex-col items-center gap-4"
+          >
+            <TabsList className="flex flex-row items-center justify-center gap-8 bg-neutral-800 font-clayno text-white">
+              <TabsTrigger
+                value="resources"
+                className="rounded-lg px-10 text-lg"
+              >
+                Resources
+              </TabsTrigger>
+              <TabsTrigger value="holders" className="rounded-lg px-10 text-lg">
+                Holders
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent
+              value="resources"
+              className="flex w-full flex-col gap-12 md:items-start"
+            >
+              <ClassCounter />
+              <MoldedMeter />
+              <MakerMeter />
+            </TabsContent>
+            <TabsContent value="holders">
+              <Holders />
+            </TabsContent>
+          </Tabs>
         </section>
       </Layout>
     </>
