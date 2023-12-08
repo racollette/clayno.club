5a0352291d0c7051df2e9671179fa8bde5388b8c
racollette <racollette@gmail.com>
2023-12-08 16:58:31 -0500

    add threads page

---
+++ src/pages/threads/index.tsx
@@ -0,0 +1,12 @@
+// make a page called threads
+// this page will display all the threads
+
+const Threads = () => {
+  return (
+    <div>
+      <h1>Threads</h1>
+    </div>
+  );
+};
+
+export default Threads;
