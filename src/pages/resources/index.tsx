c6a9acf4d255c5cb3b76d79be497292df5299171
racollette <racollette@gmail.com>
2023-12-10 17:51:34 -0500

    create resources and threads pages

--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -1,45 +1,30 @@
+import Link from "next/link";
 import Layout from "~/components/Layout";
 import MetaTags from "~/components/MetaTags";
-import { api } from "~/utils/api";
-import { Tweet } from "react-tweet";
 
-const CATEGORIES = ["bullish", "guides", "opinions"];
-
-const Threads = () => {
-  const threadCategories = api.useQueries((t) =>
-    CATEGORIES.map((category) =>
-      t.resources.getAllThreads({ categoryId: category })
-    )
-  );
-
+const Resources = () => {
   return (
     <>
-      <MetaTags title="DinoHerd | Fusion" />
+      <MetaTags title="DinoHerd | Resources" />
       <Layout>
-        <section className="flex flex-row justify-between gap-x-16">
-          <div className="flex flex-col items-center">
-            <h1 className="font-clayno text-xl">Why Bullish?</h1>
-            {threadCategories[0]?.data?.map((thread) => (
-              <div key={thread.id}>
-                <Tweet id={thread.tweetId} />
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-col items-center">
-            <h1 className="font-clayno text-xl">Guides</h1>
-            {threadCategories[1]?.data?.map((thread) => (
-              <div key={thread.id}>
-                <Tweet id={thread.tweetId} />
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-col items-center">
-            <h1 className="font-clayno text-xl">Opinions</h1>
-            {threadCategories[2]?.data?.map((thread) => (
-              <div key={thread.id}>
-                <Tweet id={thread.tweetId} />
-              </div>
-            ))}
+        <section className="flex flex-col items-center justify-center gap-y-8 p-2 md:container">
+          <div className="font-clayno text-3xl">Resources</div>
+          <div className="flex w-full flex-row justify-center gap-4">
+            <Link
+              className="min-w-2xl flex aspect-square cursor-pointer flex-col items-center justify-center gap-4 rounded-lg p-4 hover:animate-wiggle md:w-1/4"
+              style={{
+                background: `url('/images/threads.jpeg')`,
+                backgroundSize: `cover`,
+              }}
+              href={`/resources/threads`}
+            >
+              <h2 className="text-md rounded-md bg-black px-4 py-2 font-clayno md:text-lg">
+                Threads
+              </h2>
+              <p className="text-md rounded-md bg-black px-4 py-2 text-center font-clayno">
+                Explore the best X threads on Claynos!
+              </p>
+            </Link>
           </div>
         </section>
       </Layout>
@@ -47,4 +32,4 @@ const Threads = () => {
   );
 };
 
-export default Threads;
+export default Resources;
---
+++ src/pages/resources/threads.tsx
@@ -0,0 +1,50 @@
+import Layout from "~/components/Layout";
+import MetaTags from "~/components/MetaTags";
+import { api } from "~/utils/api";
+import { Tweet } from "react-tweet";
+
+const CATEGORIES = ["bullish", "guides", "opinions"];
+
+const Threads = () => {
+  const threadCategories = api.useQueries((t) =>
+    CATEGORIES.map((category) =>
+      t.resources.getAllThreads({ categoryId: category })
+    )
+  );
+
+  return (
+    <>
+      <MetaTags title="DinoHerd | Fusion" />
+      <Layout>
+        <section className="flex flex-col justify-center gap-x-4 md:flex-row md:justify-between lg:gap-x-8 xl:gap-x-16">
+          <div className="flex flex-col items-center">
+            <h1 className="font-clayno text-2xl">Why Bullish?</h1>
+            {threadCategories[0]?.data?.map((thread) => (
+              <div key={thread.id}>
+                <Tweet id={thread.tweetId} />
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col items-center">
+            <h1 className="font-clayno text-2xl">Guides</h1>
+            {threadCategories[1]?.data?.map((thread) => (
+              <div key={thread.id}>
+                <Tweet id={thread.tweetId} />
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col items-center">
+            <h1 className="font-clayno text-2xl">Opinions</h1>
+            {threadCategories[2]?.data?.map((thread) => (
+              <div key={thread.id}>
+                <Tweet id={thread.tweetId} />
+              </div>
+            ))}
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export default Threads;
