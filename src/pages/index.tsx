f23d208237287ee27d4576fac1dd08712c757414
racollette <racollette@gmail.com>
2023-12-10 17:50:35 -0500

    update landing screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             className="h-[5000px] w-[1000px] animate-homepage-dino-pattern bg-repeat md:h-[6000px] md:w-[6000px]"
           ></div>
         </div>
-        <div className="z-10 my-16 flex w-full flex-col items-center justify-around gap-16 md:gap-12">
+        <div className="z-10 my-16 flex w-full flex-col items-center justify-around gap-16 space-y-6 md:gap-12">
           <div className="flex w-full flex-col items-center justify-around gap-10 md:flex-row">
             <Link
               className="flex aspect-square w-3/4 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center text-white shadow-2xl  shadow-neutral-900 hover:animate-wiggle md:w-[30%] lg:w-[21%]"
@@ -46,8 +46,6 @@ export default function Home() {
                 {`Claynotopia's sub-communities!`}
               </h2>
             </Link>
-          </div>
-          <div className="flex w-full flex-col items-center justify-around gap-10 md:flex-row">
             <Link
               className="flex aspect-square w-3/4 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center  text-white shadow-2xl shadow-neutral-900 hover:animate-wiggle md:w-[30%] lg:w-[21%]"
               style={{
@@ -64,19 +62,36 @@ export default function Home() {
                 Data and analytics
               </h2>
             </Link>
+          </div>
+          <div className="flex w-full flex-col items-center justify-around gap-10 md:flex-row">
             <Link
               className="flex aspect-square w-3/4 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center  text-white shadow-2xl shadow-neutral-900 hover:animate-wiggle md:w-[30%] lg:w-[21%]"
               style={{
                 background: `url('/images/sticker_collage.png')`,
                 backgroundSize: `100%`,
               }}
-              href={`/fusion`}
+              href={`/tools`}
             >
               <h1 className="rounded-lg  bg-neutral-900/70 p-2 text-2xl font-black lg:text-3xl">
-                Fusion
+                Tools
               </h1>
               <h2 className="lg:text-md rounded-lg bg-neutral-900/70 p-2 text-sm font-semibold text-white">
-                An NFT video collage creation tool
+                Collage creation and more!
+              </h2>
+            </Link>
+            <Link
+              className="flex aspect-square w-3/4 -rotate-[9deg] cursor-pointer flex-col items-center justify-center gap-4 rounded-xl bg-neutral-800 bg-contain p-8 text-center  text-white shadow-2xl shadow-neutral-900 hover:animate-wiggle md:w-[30%] lg:w-[21%]"
+              style={{
+                background: `url('/images/attic.png')`,
+                backgroundSize: `contain`,
+              }}
+              href={`/resources`}
+            >
+              <h1 className="rounded-lg  bg-neutral-900/70 p-2 text-2xl font-black lg:text-3xl">
+                Resources
+              </h1>
+              <h2 className="lg:text-md rounded-lg bg-neutral-900/70 p-2 text-sm font-semibold text-white">
+                Useful links and information
               </h2>
             </Link>
           </div>
